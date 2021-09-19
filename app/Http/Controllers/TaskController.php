9072d21086ba2442896a3fef7b2c8e9e001e96c3
Herman Ceaser <ceaserbanks09@gmail.com>
2021-09-20 00:19:10 +0300

    added routes to task Resource

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
@@ -13,7 +14,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        return Task::orderBy('created_at', 'asc')->get();
     }
 
     /**
@@ -34,7 +35,16 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [ //inputs are not empty or null
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        $task = new Task;
+        $task->title = $request->input('title'); //retrieving user inputs
+        $task->description = $request->input('description');  //retrieving user inputs
+        $task->save(); //storing values as an object
+        return $task; //returns the stored value if the operation was successful.
     }
 
     /**
@@ -45,7 +55,7 @@ class TaskController extends Controller
      */
     public function show($id)
     {
-        //
+        return Task::findorFail($id);
     }
 
     /**
@@ -68,7 +78,10 @@ class TaskController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+
+        return $task;
     }
 
     /**
@@ -79,6 +92,9 @@ class TaskController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return 204;
     }
 }
--- database/seeders/TasksTableSeeder.php
+++ database/seeders/TasksTableSeeder.php
@@ -15,6 +15,17 @@ class TasksTableSeeder extends Seeder
      */
     public function run()
     {
-        Task::factory()->times(50)->create();
+        // Let's truncate our existing records to start from scratch.
+        Task::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // And now, let's create a few articles in our database:
+        for ($i = 0; $i < 50; $i++) {
+            Task::create([
+                'title' => $faker->sentence,
+                'description' => $faker->paragraph,
+            ]);
+        }
     }
 }
